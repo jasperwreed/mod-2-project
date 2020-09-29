8fa7077416ae76b329d333f7ebbc63eb1aed157a
mehmehmehlol <hkmeganlo@gmail.com>
2020-09-28 19:01:36 -0700

    Added more stuff in views page and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :reviews
   resources :product_categories
   resources :categories
-  resources :products
+  resources :products, [:index]
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
