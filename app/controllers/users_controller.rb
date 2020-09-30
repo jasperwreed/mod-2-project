bad17cd25f4c60d1f782bdd39a5cad5b47a5e976
jasperwreed <jmixon.reed@gmail.com>
2020-09-30 16:25:10 -0700

    user controller and model work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id
             redirect_to products_path
         else
-            flash[:error] = @user.errors.full_message
+            flash[:errors] = @user.errors.full_messages
             redirect_to new_user_path
         end 
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
+
+    validates :first_name, presence: true
+    validates :last_name, presence: true
     
     def full_name
         self.first_name + self.last_name
